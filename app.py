409ae082f4a576e3554798a06cece61ea71782d7
Drixel Ortega <drixelidol.ortega@deped.gov.ph>
2024-09-21 14:17:23 +0800

    Update app to use PORT environment variable

--- app.py
+++ app.py
@@ -272,5 +272,7 @@ def travel_authority():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
 
