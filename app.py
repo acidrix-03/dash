a9c5688d5509b4182e7376ef18657d1ba19d3412
Drixel Ortega <drixelidol.ortega@deped.gov.ph>
2024-09-21 16:59:39 +0800

    Bind to Heroku port and add Procfile

--- app.py
+++ app.py
@@ -320,4 +320,5 @@ def travel_authority():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))  # Get the PORT from environment, default to 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
